2bf42dd4fe4b8bdb4bb4b4b5697a5faca769f8ca
Elise HAUTEFAYE <ehautefa@e3r6p8.42.fr>
2021-06-02 15:15:33 +0200

    pb_with_volume

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -11,7 +11,12 @@ do
 done
 mysqladmin status
 
-# echo "CREATE DATABASE $DB_TMP_NAME" | mariadb -u root
+echo "CREATE DATABASE $MYSQL_DATABASE" | mariadb -u root
+echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mariadb -u root 
+echo "FLUSH PRIVILEGES;" | mariadb -u root  
+mysql -u root $MYSQL_DATABASE < tmp/wordpress.sql
+
+echo "SHOW DATABASES;" | mariadb -u root
 
 mysqladmin shutdown
 mysqld_safe --datadir="/var/lib/mysql"
