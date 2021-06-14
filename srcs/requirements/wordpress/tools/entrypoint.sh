c1ba0594fe8e5f8a609b22b776248bb9fb9b3cfc
Elise HAUTEFAYE <ehautefa@e1r11p4.42.fr>
2021-06-14 12:39:01 +0200

    php_runs_on_9000

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 5
 wp core install --url="http://ehautefa.42.fr/" --title="INCEPTION" --admin_name=elise --admin_email=ehautefa@student.42.fr --admin_password=pass --path=/var/www/html
-# php -S 0.0.0.0:9000 -t /var/www/html
-php-fpm7
+php -S 0.0.0.0:9000 -t /var/www/html
+# php-fpm7 -F
 echo "php launch"
 tail -f /var/log/php7/error.log
