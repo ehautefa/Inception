f595cf4d38aea5bf21c009c4bc5c473716e1eb6f
Elise HAUTEFAYE <ehautefa@e1r11p4.42.fr>
2021-06-14 18:19:58 +0200

    nothing_is_working_here

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-sleep 5
-wp core install --url="http://ehautefa.42.fr/" --title="INCEPTION" --admin_name=elise --admin_email=ehautefa@student.42.fr --admin_password=pass --path=/var/www/html
+echo "Wait for database"
+sleep 10
+# wp core install --url="http://ehautefa.42.fr/" --title="INCEPTION" --admin_name=elise --admin_email=ehautefa@student.42.fr --admin_password=pass --path=/var/www/html
 php -S 0.0.0.0:9000 -t /var/www/html
+echo "Starting php-fpm7 ..."
+# tail -f /var/log/php-fpm7/error.log
 # php-fpm7 -F
-echo "php launch"
-tail -f /var/log/php7/error.log
