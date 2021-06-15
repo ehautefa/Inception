5fd48ef89cc2383ae19d6bbe23126b775a353605
Elise HAUTEFAYE <ehautefa@e1r5p4.42.fr>
2021-06-15 16:29:01 +0200

    phpinfo_is_working

--- srcs/requirements/nginx/tools/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# chown -R nginx:nginx /var/
-echo "Permission /var/www/html changed"
-nginx -g "daemon on;"
-tail -f /var/log/nginx/error.log
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Wait for database"
-sleep 10
-# wp core install --url="http://ehautefa.42.fr/" --title="INCEPTION" --admin_name=elise --admin_email=ehautefa@student.42.fr --admin_password=pass --path=/var/www/html
-php -S 0.0.0.0:9000 -t /var/www/html
-echo "Starting php-fpm7 ..."
-# tail -f /var/log/php-fpm7/error.log
-# php-fpm7 -F
