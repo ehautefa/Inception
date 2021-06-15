775829726bb164387b0a5f9ed9c98a3ee5cc8cf3
Elise HAUTEFAYE <ehautefa@e1r5p4.42.fr>
2021-06-15 17:13:39 +0200

    wp_configured

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Wait for database"
+sleep 10
+wp core install --url="http://ehautefa.42.fr/" --title="INCEPTION" --admin_name=elise --admin_email=ehautefa@student.42.fr --admin_password=pass --path=/srcs/
+echo "Starting php-fpm7 ..."
+php-fpm7 -F
