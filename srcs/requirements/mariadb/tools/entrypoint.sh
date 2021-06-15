b2119f1c9c305f5e996ba98ffa6a7dfe8f6327d8
Elise HAUTEFAYE <ehautefa@e1r5p4.42.fr>
2021-06-15 18:07:56 +0200

    css_ok

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -7,6 +7,7 @@ do
 done
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE" | mariadb -u root
 echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" | mariadb -u root 
+echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER_2' IDENTIFIED BY '$MYSQL_PASSWORD_2' WITH GRANT OPTION;" | mariadb -u root 
 echo "FLUSH PRIVILEGES;" | mariadb -u root  
 mysql -u root $MYSQL_DATABASE < tmp/wordpress.sql
 mysqladmin shutdown
